3577dab674fd7900308d9344f92cff026a6b89ce
adrianmahk <45867342+adrianmahk@users.noreply.github.com>
2021-10-30 03:16:53 +0800

    Add files via upload
    [dev] loadingbar: use animationiterationend event instead

--- blog-ui-ajax-dev.js
+++ blog-ui-ajax-dev.js
@@ -9,9 +9,6 @@ function hidePageLoading(delay = 1000) {
   if (delay > 0) {
     document.body.classList.remove('page-loading');
     document.body.classList.add('page-loading-end');
-    setTimeout(() => {
-      document.body.classList.remove('page-loading-end');
-    }, delay);
   }
   else {
     document.body.classList.remove('page-loading');
@@ -169,6 +166,13 @@ function init() {
         document.body.setAttribute("orientation", ori);
       }
       drawButtonsShadow();
+    });
+    window.addEventListener('animationiteration', function(event) {
+      if (event.target.classList.contains('loading-bar')) {
+        if (document.body.classList.contains('page-loading-end')) {
+          document.body.classList.remove('page-loading-end');
+        }
+      }
     });
     loadIndie();
     loadScrollPos();
